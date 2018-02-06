2932c7edc77d4f42808e1805c611a21e2b1e825e
wangbaojin <904026435@qq.com>
2018-02-06 12:29:52 +0800

    测试

---
+++ Application/api/Controller/EggcoinUserController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wangbaojin
+ * Date: 18/2/6
+ * Time: 下午12:15
+ */
+class EggcoinUserController
+{
+    public  function test(){
+
+        echo "lianyangji";
+    }
+
+
+}
